81c4a6681f50b42baf47bf4e0fe2ec4906d4c0c0
Wasse Lucas <wasselucas.dev@gmail.com>
2023-01-03 14:20:32 -0300

    problema exemplo - while

---
+++ src/estruturas_repetitivas/ProblemaEx.java
@@ -0,0 +1,23 @@
+package estruturas_repetitivas;
+
+import java.util.Scanner;
+
+public class ProblemaEx {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        int soma = 0;
+
+        while (x != 0){
+            soma += x;
+            x = sc.nextInt();
+        }
+
+        System.out.print(soma);
+
+        sc.close();
+    }
+}
