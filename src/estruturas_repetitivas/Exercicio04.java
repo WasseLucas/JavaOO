f8d03c7456b9009bfb2dd10aae345f0733c543f8
Wasse Lucas <wasselucas.dev@gmail.com>
2023-01-03 18:52:36 -0300

    exercicio03 teste de  mesa  - while

---
+++ src/estruturas_repetitivas/Exercicio04.java
@@ -0,0 +1,25 @@
+package estruturas_repetitivas;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = 0;
+
+        while (x < 5){
+           int y = x * 3;
+
+            System.out.print(y);
+
+            x = x + 1;
+        }
+
+
+        System.out.println("Fim");
+
+        sc.close();
+    }
+}
