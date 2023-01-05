9a73e35a2111791ebe86009b81b3e3c3de9a346c
Wasse Lucas <wasselucas.dev@gmail.com>
2023-01-05 14:36:46 -0300

    exercicio06 teste de mesa - while

---
+++ src/estruturas_repetitivas/Exercicio06.java
@@ -0,0 +1,20 @@
+package estruturas_repetitivas;
+
+public class Exercicio06 {
+    public static void main(String[] args) {
+
+        int x = 2;
+        int y = 10;
+
+        System.out.println("Olá");
+
+        while (x < y){
+
+            System.out.println( x + "-" + y);
+
+            x = x * 2;
+            y = y + 1;
+
+        }
+    }
+}
