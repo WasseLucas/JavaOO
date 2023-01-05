b499787660593d79bc0fc01775081fdf99f0e7a6
Wasse Lucas <wasselucas.dev@gmail.com>
2023-01-05 17:27:01 -0300

    problema exemplo for

---
+++ src/estruturas_repetitivas/PreoblemaExFor.java
@@ -0,0 +1,25 @@
+package estruturas_repetitivas;
+
+import java.util.Scanner;
+
+public class PreoblemaExFor {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int soma = 0;
+
+        for ( int i = 0; i < N; i++ ){
+
+            int x = sc.nextInt();
+            soma = soma + x;
+
+        }
+
+        System.out.println(soma);
+
+        sc.close();
+    }
+}
