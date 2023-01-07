ce5f7198f813574ff6a81a3264e5064bdd02b52f
Wasse Lucas <wasselucas.dev@gmail.com>
2023-01-07 14:03:32 -0300

    exercicio05 teste de mesa manual - for

---
+++ src/estruturas_repetitivas/Exercicio05For.java
@@ -0,0 +1,14 @@
+package estruturas_repetitivas;
+
+public class Exercicio05For {
+    public static void main(String[] args) {
+
+        int x = 4;
+        int y = 0;
+
+        for (int i = 0; i < x; i++){
+            y = y + i;
+        }
+        System.out.println(y);
+    }
+}
