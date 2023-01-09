27fa346aff336645c6e3ea32718e8d65af90c1f8
Wasse Lucas <wasselucas.dev@gmail.com>
2023-01-08 22:55:47 -0300

    exercicio 07 for

--- src/exercicio_for/Exercicio07.java
+++ src/exercicio_for/Exercicio07.java
@@ -3,6 +3,10 @@ package exercicio_for;
 import java.util.Locale;
 import java.util.Scanner;
 
+//Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
+//começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor, conforme
+//exemplo.
+
 public class Exercicio07 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
