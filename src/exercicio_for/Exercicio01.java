7ace2f5ad81b5629edcf24f9f075f832bdf8c0b8
Wasse Lucas <wasselucas.dev@gmail.com>
2023-01-08 22:54:53 -0300

    exercicio 01 for

--- src/exercicio_for/Exercicio01.java
+++ src/exercicio_for/Exercicio01.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 //Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
 //X, se for o caso.
 
-public class Exercicio1 {
+public class Exercicio01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
