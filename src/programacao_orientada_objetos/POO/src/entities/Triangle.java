6941cc4b0f52559efaf3b28d41c8acf78261aa32
Wasse Lucas <wasselucas.dev@gmail.com>
2023-01-17 12:55:32 -0300

    criando metodo para calcular area de um triangulo

--- src/programacao_orientada_objetos/POO/src/entities/Triangle.java
+++ src/programacao_orientada_objetos/POO/src/entities/Triangle.java
@@ -7,4 +7,11 @@ public class Triangle {
     public double a;
     public double b;
     public double c;
+
+    //Função para calcular area de um triangulo
+    public double area(){
+        double p = (a + b + c)/2.0;
+        return Math.sqrt(p * (p-a)*(p-b)*(p-c));
+    };
+
 }
