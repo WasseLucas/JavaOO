ff1045c1d68137113a402517a3afedc29b226c6b
Wasse Lucas <wasselucas.dev@gmail.com>
2023-01-17 11:37:59 -0300

    Resolvendo exercicio ultilizando orientacao objeto

--- src/programacao_orientada_objetos/POO/src/Program.java
+++ src/programacao_orientada_objetos/POO/src/Program.java
@@ -3,6 +3,14 @@ import entities.Triangle;
 import java.util.Locale;
 import java.util.Scanner;
 
+/*
+        Fazer um programa para ler as medidas dos lados de dois triângulos X e Y (suponha medidas
+        válidas). Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triângulos
+        possui a maior área.
+        A fórmula para calcular a área de um triângulo a partir das medidas de seus lados a, b e c é a
+        seguinte (fórmula de Heron).
+ */
+
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
