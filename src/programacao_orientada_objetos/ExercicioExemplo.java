c93a88a0a3d407f8dee4afed0fe766a6395ab4e1
Wasse Lucas <wasselucas.dev@gmail.com>
2023-01-17 10:22:27 -0300

    resolvendo exercicio sem orientaçcao objeto

---
+++ src/programacao_orientada_objetos/ExercicioExemplo.java
@@ -0,0 +1,51 @@
+package programacao_orientada_objetos;
+
+//Resolvendo exercicio sem orientação objeto
+
+/*
+        Fazer um programa para ler as medidas dos lados de dois triângulos X e Y (suponha medidas
+        válidas). Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triângulos
+        possui a maior área.
+        A fórmula para calcular a área de um triângulo a partir das medidas de seus lados a, b e c é a
+        seguinte (fórmula de Heron).
+ */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExercicioExemplo {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double xA, xB, xC, yA,yB,yC;
+
+        System.out.println("Enter the measures of triangle x: ");
+        xA = sc.nextDouble();
+        xB = sc.nextDouble();
+        xC = sc.nextDouble();
+
+        System.out.println("Enter the measures of triangle y: ");
+        yA = sc.nextDouble();
+        yB = sc.nextDouble();
+        yC = sc.nextDouble();
+
+        double p = (xA + xB + xC)/2.0;
+        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+        p = (yA + yB + yC)/2.0;
+        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+        System.out.printf("Triangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle y area: %.4f%n", areaY);
+
+        if (areaX > areaY){
+            System.out.println("Larger area: X");
+        }
+        else {
+            System.out.println("Larger area: Y");
+        }
+
+        sc.close();
+    }
+}
