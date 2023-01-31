21008f8cd9a10ebf43d5f90e7363fe20e712a829
Wasse Lucas <wasselucas.dev@gmail.com>
2023-01-31 12:03:00 -0300

    modulo encapsulamento

--- src/construtores_sobrecarga_encapsulamento/construtores/ProblemaExemplo/src/entities/Product.java
+++ src/construtores_sobrecarga_encapsulamento/construtores/ProblemaExemplo/src/entities/Product.java
@@ -2,16 +2,48 @@ package entities;
 
 public class Product {
 
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
 
-    //constutor
+    public Product(){}
+
+
     public Product(String name, double price, int quantity){
-        this.name = this.name;
-        this.price = this.price;
-        this.quantity = this.quantity;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
     }
+
+    public Product(String name, double price){
+        this.name = name;
+        this.price = price;
+
+    }
+
+    //Get e Setters
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+
 
     public double totalValueInStock(){
         return  price * quantity;
@@ -23,5 +55,15 @@ public class Product {
 
     public void  removeProducts(int quantity){
         this.quantity = quantity - quantity;
+    }
+
+    public String toString() {
+        return name
+                + ", $ "
+                + String.format("%.2f", price)
+                + ", "
+                + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValueInStock());
     }
 }
