39e0f3f9b3ffc773979318afed097105086b0f81
Wasse Lucas <wasselucas.dev@gmail.com>
2023-02-06 17:20:55 -0300

    programa para ler uma matriz de m linha e n colunas

---
+++ src/memoria_array_listas/listas/Exercicio/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+//Fazer um programa para ler dois números inteiros M e N, e depois ler
+//uma matriz de M linhas por N colunas contendo números inteiros,
+//podendo haver repetições. Em seguida, ler um número inteiro X que
+//pertence à matriz. Para cada ocorrência de X, mostrar os valores à
+//esquerda, acima, à direita e abaixo de X, quando houver, conforme
+//exemplo.
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] mat = new int[m][n];
+
+        for (int i=0; i<mat.length; i++) {
+            for (int j=0; j<mat[i].length; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        int x = sc.nextInt();
+
+        for (int i=0; i<mat.length; i++) {
+            for (int j=0; j<mat[i].length; j++) {
+                if (mat[i][j] == x) {
+                    System.out.println("Position " + i + "," + j + ":");
+                    if (j > 0) {
+                        System.out.println("Left: " + mat[i][j-1]);
+                    }
+                    if (i > 0) {
+                        System.out.println("Up: " + mat[i-1][j]);
+                    }
+                    if (j < mat[i].length-1) {
+                        System.out.println("Right: " + mat[i][j+1]);
+                    }
+                    if (i < mat.length-1) {
+                        System.out.println("Down: " + mat[i+1][j]);
+                    }
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
