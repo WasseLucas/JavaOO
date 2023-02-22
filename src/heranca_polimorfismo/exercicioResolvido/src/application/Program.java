dd88e859a8561ce8b548abc9dea759be7bbb2e0e
Wasse Lucas <wasselucas.dev@gmail.com>
2023-02-22 16:22:20 -0300

    exercicio resolvido

--- src/heranca_polimorfismo/exercicioResolvido/src/application/Program.java
+++ src/heranca_polimorfismo/exercicioResolvido/src/application/Program.java
@@ -8,6 +8,19 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
+//Uma empresa possui funcionários próprios e terceirizados.
+//Para cada funcionário, deseja-se registrar nome, horas
+//trabalhadas e valor por hora. Funcionários terceirizado
+//possuem ainda uma despesa adicional.
+//O pagamento dos funcionários corresponde ao valor da hora
+//multiplicado pelas horas trabalhadas, sendo que os
+//funcionários terceirizados ainda recebem um bônus
+//correspondente a 110% de sua despesa adicional.
+//Fazer um programa para ler os dados de N funcionários (N
+//fornecido pelo usuário) e armazená-los em uma lista. Depois
+//de ler todos os dados, mostrar nome e pagamento de cada
+//funcionário na mesma ordem em que foram digitados.
+
 public class Program {
     public static void main(String[] args) {
 
