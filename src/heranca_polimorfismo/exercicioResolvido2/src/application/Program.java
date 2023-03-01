9dd122677ce81dcbef18bd877185dbdd7fd5673b
Wasse Lucas <wasselucas.dev@gmail.com>
2023-03-01 14:46:30 -0300

    exercicio fresolvido

--- src/heranca_polimorfismo/exercicioResolvido2/src/application/Program.java
+++ src/heranca_polimorfismo/exercicioResolvido2/src/application/Program.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
+
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
